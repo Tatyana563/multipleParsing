96b5ee62542dc5c3f711bc3926f199c4f3671dc7
Tanya <tatyana.petrenko@dstarlab.com>
2020-11-06 11:36:19 +0200

    Configuration Properties changed

--- esp-app/src/main/java/ua/tpetrenko/esp/ESPApplication.java
+++ esp-app/src/main/java/ua/tpetrenko/esp/ESPApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import ua.tpetrenko.esp.api.ConfigProperties;
 
 /**
  * @author Roman Zdoronok
  */
 
 @SpringBootApplication
-@EnableConfigurationProperties
+@EnableConfigurationProperties(ConfigProperties.class)
 public class ESPApplication {
 
     public static void main(String[] args) {
